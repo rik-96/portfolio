e4ae5b6da1711f57bc224869e5f805a306a6984f
rik <dk84864@gmail.com>
2018-09-05 20:32:46 +0530

    added skills and projects section

--- anim.js
+++ anim.js
@@ -6,12 +6,12 @@ $(document).ready(function(){
    return Math.round(Math.random() * (max - min + 1)) + min;
  }
   
-  $('#about-me-sectn').mousemove(function(e) {
+  $('.animm').mousemove(function(e) {
     mousePos.x = e.pageX;
     mousePos.y = e.pageY;
   });
   
-  $('#about-me-sectn').mouseleave(function(e) {
+  $('.animm').mouseleave(function(e) {
     mousePos.x = -1;
     mousePos.y = -1;
   });
@@ -23,7 +23,7 @@ $(document).ready(function(){
       
       var color = "background: rgb("+getRandomInt(0,255)+","+getRandomInt(0,255)+","+getRandomInt(0,255)+");";
       
-      var sizeInt = getRandomInt(10, 30);
+      var sizeInt = getRandomInt(5, 20);
       size = "height: " + sizeInt + "px; width: " + sizeInt + "px;";
       
       var left = "left: " + getRandomInt(mousePos.x-range-sizeInt, mousePos.x+range) + "px;";
@@ -31,7 +31,46 @@ $(document).ready(function(){
       var top = "top: " + getRandomInt(mousePos.y-range-sizeInt, mousePos.y+range) + "px;"; 
 
       var style = left+top+color+size;
-      $("<div class='ball' style='" + style + "'></div>").appendTo('#about-me-sectn').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){$(this).remove();}); 
+      $("<div class='ball' style='" + style + "'></div>").appendTo('.animm').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){$(this).remove();}); 
+    }
+  }, 1);
+});
+
+
+$(document).ready(function(){
+  
+  var mousePos = {};
+
+ function getRandomInt(min, max) {
+   return Math.round(Math.random() * (max - min + 1)) + min;
+ }
+  
+  $('.animmm').mousemove(function(e) {
+    mousePos.x = e.pageX;
+    mousePos.y = e.pageY;
+  });
+  
+  $('.animmm').mouseleave(function(e) {
+    mousePos.x = -1;
+    mousePos.y = -1;
+  });
+  
+  var draw = setInterval(function(){
+    if(mousePos.x > 0 && mousePos.y > 0){
+      
+      var range = 15;
+      
+      var color = "background: rgb("+getRandomInt(0,255)+","+getRandomInt(0,255)+","+getRandomInt(0,255)+");";
+      
+      var sizeInt = getRandomInt(5, 20);
+      size = "height: " + sizeInt + "px; width: " + sizeInt + "px;";
+      
+      var left = "left: " + getRandomInt(mousePos.x-range-sizeInt, mousePos.x+range) + "px;";
+      
+      var top = "top: " + getRandomInt(mousePos.y-range-sizeInt, mousePos.y+range) + "px;"; 
+
+      var style = left+top+color+size;
+      $("<div class='ball' style='" + style + "'></div>").appendTo('.animm').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){$(this).remove();}); 
     }
   }, 1);
 });
