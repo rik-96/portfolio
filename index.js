7539bd88121baecbae252803d261e2b59ab440d2
rik <dk84864@gmail.com>
2018-09-06 21:47:00 +0530

    adding script to prevent resize of background on mobile on scroll

--- index.js
+++ index.js
@@ -2,20 +2,15 @@ setTimeout(function(){
   $('#skills').fadeIn();
 }, 2000);
 
-function res(){
-  var els = document.querySelectorAll('.vh-fix')
-  if (!els.length) return
-
-  for (var i = 0; i < els.length; i++) {
-    var el = els[i]
-    if (el.nodeName === 'IMG') {
-      el.onload = function() {
-        this.style.height = this.clientHeight + 'px'
-      }
-    } else {
-      el.style.height = el.clientHeight + 'px'
+var breakpoint = 991;
+var bgHeight = function() {
+    $('#initbackground').css('height', $(window).height() + 'px');
+}; bgHeight();
+var windowWidth = $(window).height();
+$(window).on('resize', function() {
+    if ((($(this).width() <= breakpoint) && ($(this).width() != windowWidth))
+        || ($(this).width() > breakpoint)) {
+        bgHeight();
     }
-  }
-}
-
-res();
+    windowWidth = $(window).width();
+});
