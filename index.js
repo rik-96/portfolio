a3ffd69ca8109eeca8913cb9911f2d965cae5982
rik <dk84864@gmail.com>
2018-09-07 13:06:43 +0530

    added responsive background for laptops

--- index.js
+++ index.js
@@ -2,16 +2,3 @@ setTimeout(function(){
   $('#skills').fadeIn();
 }, 2000);
 
-var breakpoint = 991;
-var bgHeight = function() {
-    $('#initbackground').css('height', $(window).height() + 'px');
-}; bgHeight();
-var windowWidth = $(window).height();
-$(window).on('resize', function() {
-    if ((($(this).width() <= breakpoint) && ($(this).width() != windowWidth))
-        || ($(this).width() > breakpoint)) {
-        bgHeight();
-    }
-    windowWidth = $(window).width();
-});
-
