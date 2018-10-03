a5d4465f331c8427864ad75f3ffcc7257376c01d
rik <dk84864@gmail.com>
2018-10-03 21:22:57 +0530

    changed layout

--- anim.js
+++ anim.js
@@ -53,3 +53,45 @@ $(document).ready(function(){
     }
   }, 1);
 });
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  $('#'+slides[slideIndex-1].id).fadeIn();
+}
+
+/*var offset = $('#my-project-sectn').position().top;
+var off = $('#my-project-sectn').position().top + $('#my-project-sectn').outerHeight(true);
+var navbar = $('#navbar');
+var $window = $(window);
+
+$window.scroll(function() {
+  if ( $window.scrollTop() > offset && $window.scrollTop() < off )
+    navbar.css('background-color', 'red');
+  else
+    navbar.css('background-color', 'blue');
+});
+
+console.log(off);
+
+console.log(offset);*/
